6b478e191cc28c076667d6f8eadfc617a58131be
CauaneTemporin <temporin.cauane@gmail.com>
2024-02-25 19:43:36 -0300

    Criação do projeto SQLAutoImporter

---
+++ sqlautoimporter/src/main/java/com/temporintech/sqlautoimporter/SqlAutoImporterApplication.java
@@ -0,0 +1,13 @@
+package com.temporintech.sqlautoimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlAutoImporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlAutoImporterApplication.class, args);
+	}
+
+}
---
+++ sqlautoimporter/src/test/java/com/temporintech/sqlautoimporter/SqlAutoImporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temporintech.sqlautoimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlAutoImporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
